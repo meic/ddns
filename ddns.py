30902c8873dbd1ab8af8969843eb0a7238b02af6
Meic Williams <1534923+meic@users.noreply.github.com>
2021-09-09 15:39:41 +0100

    Refactor and update

--- ddns.py
+++ ddns.py
@@ -3,66 +3,64 @@ import requests
 import smtplib
 from dns import resolver
 
-IPV4_INFO_URL = 'http://ifconfig.co/json'
-CONFIG_FILE = 'config.json'
 
+class Ddns:
+    IPV4_INFO_URL = "http://ifconfig.co/json"
+    CONFIG_FILE = "config.json"
 
-def get_config():
-    return json.load(open(CONFIG_FILE))
+    def __init__(self):
+        self.config = json.load(open(self.CONFIG_FILE))
 
+    def get_old_ip(self):
+        res = resolver.Resolver()
+        res.nameservers = ["1.1.1.1"]
+        answers = res.resolve(self.config["hostname"])
+        for rdata in answers:
+            return rdata.address
 
-def get_old_ip():
-    res = resolver.Resolver()
-    res.nameservers = ['1.1.1.1']
-    answers = res.query('home.meic.dev')
-    for rdata in answers:
-        return rdata.address
-
-def send_email(config, ip):
-    smtp = smtplib.SMTP('localhost')
-    smtp.sendmail(
-        config['email_from'],
-        config['email_to'],
-        'Subject: Home IP changed\n\nIP changed to: {ip}'.format(
-            ip=ip
+    def send_email(self, ip):
+        smtp = smtplib.SMTP("localhost")
+        smtp.sendmail(
+            self.config["email_from"],
+            self.config["email_to"],
+            "Subject: Home IP changed\n\nIP changed to: {ip}".format(ip=ip),
         )
-    )
 
-def main():
-    req = requests.get(url=IPV4_INFO_URL)
+    def run(self):
+        req = requests.get(url=self.IPV4_INFO_URL)
 
-    if not req.ok:
-        return
-    ip = req.json()['ip']
-    old_ip = get_old_ip()
-    if ip == old_ip:
-        return
+        if not req.ok:
+            return
+        ip = req.json()["ip"]
+        old_ip = self.get_old_ip()
+        if ip == old_ip:
+            return
 
-    config = get_config()
+        url = (
+            "https://api.cloudflare.com/client/v4/zones/{zone_identifier}"
+            "/dns_records/{identifier}"
+        ).format(
+            zone_identifier=self.config["zone_id"],
+            identifier=self.config["dns_record_id"],
+        )
 
-    url = ('https://api.cloudflare.com/client/v4/zones/{zone_identifier}'
-           '/dns_records/{identifier}').format(
-        zone_identifier=config['zone_id'],
-        identifier=config['dns_record_id'],
-    )
+        req = requests.put(
+            url=url,
+            headers={
+                "X-Auth-Email": self.config["api_email"],
+                "X-Auth-Key": self.config["api_key"],
+            },
+            json={
+                "type": "A",
+                "name": self.config["hostname"],
+                "content": ip,
+                "ttl": 120,
+                "proxied": False,
+            },
+        )
 
-    req = requests.put(
-        url=url,
-        headers={
-            'X-Auth-Email': config['api_email'],
-            'X-Auth-Key': config['api_key'],
-        },
-        json={
-            'type': 'A',
-            'name': 'home.meic.dev',
-            'content': ip,
-            'ttl': 120,
-            'proxied': False,
-        }
-    )
-
-    send_email(config, ip)
+        self.send_email(ip)
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    Ddns().run()
